2944dcd552bb8be11c4d9196023ec743b6a620b7
Showan Asyabi <easyabi@bu.edu>
2021-11-12 10:45:47 -0500

    add config.txt

--- build/tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/showanasyabi/Desktop/trash/caspGadget/Gadget/tests/lib/googletest
-# Build directory: /Users/showanasyabi/Desktop/trash/caspGadget/Gadget/build/tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
