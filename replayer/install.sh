558a2109717effd7bfbca782dd4b812659a8021f
pentium3 <celerond@msn.com>
2022-02-10 21:33:21 -0500

    Update install.sh

--- replayer/install.sh
+++ replayer/install.sh
@@ -1,12 +1,19 @@
 # replayer
 
 ## Install dependencies
-#apt update
-#apt-get install vim net-tools ssh openssh-server -y
-#apt-get install p7zip-full sysstat python3 python3-pip cmake unzip tar git gcc-8 g++-8 openjdk-11-jre-headless libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libaio-dev uuid-dev libtbb-dev libssl-dev libboost-all-dev -y
-#ln -s /usr/bin/g++-8 /usr/bin/g++
-#ln -s /usr/bin/gcc-8 /usr/bin/gcc
-#pip3 install matplotlib
+apt update
+apt-get install vim net-tools ssh openssh-server -y
+apt-get install p7zip-full sysstat python3 python3-pip cmake unzip tar git gcc-8 g++-8 openjdk-11-jre-headless libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libaio-dev uuid-dev libtbb-dev libssl-dev libboost-all-dev -y
+ln -s /usr/bin/g++-8 /usr/bin/g++
+ln -s /usr/bin/gcc-8 /usr/bin/gcc
+pip3 install matplotlib
+
+## clean
+rm -rf rocksdb6114
+rm -rf ycsb-0.17.0-rocksdb
+rm -rf FASTER
+rm -rf lethe
+rm -rf berkerlydb
 
 
 #### YCSB
