9893e532e37e31513ac0f607d8307915bbea14dc
Showan Asyabi <easyabi@bu.edu>
2022-02-24 18:13:56 -0500

    add g++ shells

---
+++ install_build_essentals.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt-get install vim net-tools ssh openssh-server -y
+sudo apt-get install p7zip-full sysstat python3 python3-pip cmake unzip tar git gcc-8 g++-8 openjdk-11-jre-headless libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libaio-dev uuid-dev libtbb-dev libssl-dev libboost-all-dev -y
+sudo ln -s /usr/bin/g++-8 /usr/bin/g++
+sudo ln -s /usr/bin/gcc-8 /usr/bin/gcc
